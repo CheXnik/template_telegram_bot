2cf258ff9afbfb6c421301dff973eef3eb395bd8
Nikita <75634113+CheXnik@users.noreply.github.com>
2022-07-13 14:58:45 +0300

    Update admin.py

--- handlers/admin.py
+++ handlers/admin.py
@@ -1,4 +1,13 @@
-from loader import dp, bot
+import logging
+
+from aiogram import types
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
+
 from data.config import ADMIN_ID
+from loader import dp, bot
+
+
+logging.basicConfig(level=logging.INFO)
+dp.middleware.setup(LoggingMiddleware())
 
 # Хендлер для функций администрирования
